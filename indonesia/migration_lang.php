ac38a17f66483fc5ac913319649fe9aee36f5adf
Devid Haryalesmana <devid.wahid@gmail.com>
2013-11-15 05:08:14 +0700

    update migration_language.php

--- indonesia/migration_lang.php
+++ indonesia/migration_lang.php
@@ -1,12 +1,12 @@
 <?php
 
-$lang['migration_none_found']			= "No migrations were found.";
-$lang['migration_not_found']			= "This migration could not be found.";
-$lang['migration_multiple_version']		= "This are multiple migrations with the same version number: %d.";
-$lang['migration_class_doesnt_exist']	= "The migration class \"%s\" could not be found.";
-$lang['migration_missing_up_method']	= "The migration class \"%s\" is missing an 'up' method.";
-$lang['migration_missing_down_method']	= "The migration class \"%s\" is missing an 'down' method.";
-$lang['migration_invalid_filename']		= "Migration \"%s\" has an invalid filename.";
+$lang['migration_none_found']			= "Tidak ada migrasi yang ditemukan.";
+$lang['migration_not_found']			= "Migrasi ini tidak ditemukan.";
+$lang['migration_multiple_version']		= "Terdapat multi-migrasi dengan nomor versi yang sama: %d.";
+$lang['migration_class_doesnt_exist']	= "Migration class \"%s\" tidak ditemukan.";
+$lang['migration_missing_up_method']	= "Migration class \"%s\" tidak terdapat 'up' method.";
+$lang['migration_missing_down_method']	= "Migration class \"%s\" tidak terdapat 'down' method.";
+$lang['migration_invalid_filename']		= "Nama Migrasi \"%s\" tidak sah.";
 
 
 /* End of file migration_lang.php */
